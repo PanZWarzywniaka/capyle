76420dd428762b7dc82fb38f64ef393cd26c97ff
tomsmith01 <thomas.smith2110@gmail.com>
2022-11-14 16:22:26 +0000

    Town state, transition function WIP

--- ca_descriptions/wildfire_2d.py
+++ ca_descriptions/wildfire_2d.py
@@ -36,15 +36,16 @@ def transition_func(grid, neighbourstates, neighbourcounts):
     # 1 - canyon
     # 2 - forest
     # 3 - lake
+    # 4 - town
     # other:
-    # 4 - burning
-    # 5 - completely burned
+    # 5 - burning
+    # 6 - completely burned
 
-    # if a cell is burning (state 4), it will be completely burned down (state 5) in the next timestep
-    burning_cells = (grid == 4)
+    # if a cell is burning (state 5), it will be completely burned down (state 6) in the next timestep
+    burning_cells = (grid == 5)
 
-    # if at least one nearest-neighbour of a cell is burning (state 4) then catch fire with probability Pburn,
-    at_least_one_burning_neighbour = (neighbourcounts[4] > 0)
+    # if at least one nearest-neighbour of a cell is burning (state 5) then catch fire with probability Pburn,
+    at_least_one_burning_neighbour = (neighbourcounts[5] > 0)
 
     p_burn_chap = P_0 * (1 + P_VEG['chaparral']) * P_W
     p_burn_canyon = P_0 * (1 + P_VEG['canyon']) * P_W
@@ -66,13 +67,37 @@ def transition_func(grid, neighbourstates, neighbourcounts):
     to_burning_state_for = forest_cells & at_least_one_burning_neighbour & probability_arr_for
     to_burning_state_lake = lake_cells & at_least_one_burning_neighbour & probability_arr_lake
 
-    grid[burning_cells] = 5
-    grid[to_burning_state_chap] = 4
-    grid[to_burning_state_can] = 4
-    grid[to_burning_state_for] = 4
-    grid[to_burning_state_lake] = 4
+    grid[burning_cells] = 6
+    grid[to_burning_state_chap] = 5
+    grid[to_burning_state_can] = 5
+    grid[to_burning_state_for] = 5
+    grid[to_burning_state_lake] = 5
 
     return grid
+
+    #This function does not currently account for fuel and burn time
+def new_transition_func(grid, neighbourstates, neighbourcounts):
+    burning_cells = (grid == 4)
+
+    num_burning_neighbours = neighbourcounts[5]
+
+    nw, n, ne, w, e, sw, s, se = neighbourstates
+
+    nw_wind_prob, ne_wind_prob = np.degrees()
+
+    nw_burning_cells = (nw == 5)
+    n_burning_cells = (n == 5)
+    ne_burning_cells = (ne == 5)
+    w_burning_cells = (w == 5)
+    e_burning_cells = (e == 5)
+    sw_burning_cells = (sw == 5)
+    s_burning_cells = (s == 5)
+    se_burning_cells = (se == 5)
+
+
+
+
+    pass
 
 
 def setup(args):
@@ -81,15 +106,17 @@ def setup(args):
     # ---THE CA MUST BE RELOADED IN THE GUI IF ANY OF THE BELOW ARE CHANGED---
     config.title = "Wildfire simulation"
     config.dimensions = 2
-    config.states = (0, 1, 2, 3, 4, 5)
+    config.states = (0, 1, 2, 3, 4, 5, 6)
+    config.grid_dims = (40, 40)
     # ------------------------------------------------------------------------
 
 
     colors = {
-        "chapparal" : (0.5, 0.5, 0.5),
+        "chapparal" : (1.0, 0.8, 0),
         "canyon" : (1.0, 0.5, 0.5),
         "forest" : (0.0, 1.0, 0.0),
         "lake" : (0.0, 0.0, 1.0),
+        "town" : (0.7, 0.3, 0.0),
         "burning" : (1.0, 0.0, 0.0),
         "burned" : (0.0, 0.0, 0.0)
     }
