e01ebc20c5f20595ab3a9517abe5a0c395c29ea2
Aleksander Osikowicz <amosikowicz1@sheffield.ac.uk>
2022-12-01 12:48:40 +0000

    Refactor change comments

--- ca_descriptions/wildfire_2d.py
+++ ca_descriptions/wildfire_2d.py
@@ -21,7 +21,8 @@ from capyle.ca import Grid2D, Neighbourhood, CAConfig, randomise2d
 # ---
 
 # constants
-P_0 = 0.58
+SCALE_FACTOR = 0.2
+P_0 = 0.58 * SCALE_FACTOR
 P_W = 1.0
 P_VEG = {'chaparral': 0.2, 'canyon': 0.9,
          'forest': -0.5, 'lake': -1.0, 'town': 1.0}
@@ -78,8 +79,8 @@ def is_town_reached(grid) -> bool:
 
 
 def transition_func(grid, neighbourstates, neighbourcounts, fuel_grid, reached_town,counter):
-    counter += 1
-    # not burning terrain types:
+
+    # terrain types:
     # 0 - chaparral
     # 1 - canyon
     # 2 - forest
@@ -87,20 +88,25 @@ def transition_func(grid, neighbourstates, neighbourcounts, fuel_grid, reached_t
     # 4 - town
     # other:
     # 5 - burning
-    # 6 - completely burned
+    # 6 - dead
 
+    counter += 1
     # if a cell is burning (state 5), it will be completely burned down (state 6)
     # after number of steps equal to the amount of fuel for the cell.
     burning_cells = (grid == 5)
     fuel_grid[burning_cells] -= 1
-    dead_cells = (fuel_grid == 0)
 
-    # if at least one nearest-neighbour of a cell is burning (state 5) then catch fire with probability Pburn,
-    # at_least_one_burning_neighbour = (neighbourcounts[5] > 0)
+
+
+
+
+    # set up grid with base burn probabilities for each cell
+    ignite_prob_grid = setup_ignite_probabilities_grid(grid)
 
     # calculate wind effect
-    # NW, N, NE, W, E, SW, S, SE = neighbourstates
     wind_prob_grid = np.zeros(grid.shape)
+
+    #for each direction calculate ignition propability dependant on wind direction
     for (direction, cos_a) in zip(neighbourstates, COS_VALS[WIND_DIR]):
         curr_direction_burning = (direction == 5)
 
@@ -110,29 +116,34 @@ def transition_func(grid, neighbourstates, neighbourcounts, fuel_grid, reached_t
         wind_prob_grid[curr_direction_burning] += math.exp(
             V*(C_1 + C_2*(cos_a - 1)))
 
-    # set up grid with base burn probabilities for each cell
-    ignite_prob_grid = setup_ignite_probabilities_grid(grid)
 
     # Note: if a cell doesn't have any burning neighbours, then
     # it will have value zero in wind_prob_grid, so it will also zero its
     # value in ignite_prob_grid after the multiplication below, so we don't
-    # need at_least_one_burning_neighbour array (NOT SURE VERIFY IT!!!)
     # multiply base burn probabilities by summed P_W from each direction
     ignite_prob_grid *= wind_prob_grid
 
+    #generate random numbers to test against propabilities
     random_grid = np.random.rand(*grid.shape)
+
+    #identify cells to ignite
     cells_to_ignite = random_grid < ignite_prob_grid
+    #set those cells to burning state
+    grid[cells_to_ignite] = 5
 
-    to_burning_state = cells_to_ignite  # & at_least_one_burning_neighbour
+    #identify cells which ran out of fuel
+    dead_cells = (fuel_grid == 0)
+    #set them to dead state
+    grid[dead_cells] = 6
 
+    #do water drop at intervention time
     if counter[0] == INTERVENTION_TIME:
         water_grid = np.full(grid.shape, False)
         water_grid[WATER_DROP_COORDS[0]:WATER_DROP_COORDS[1],WATER_DROP_COORDS[2]:WATER_DROP_COORDS[3]] = True
         grid[water_grid] = 3
 
-    grid[to_burning_state] = 5
-    grid[dead_cells] = 6
 
+    #check if we reached the town
     if not np.any(reached_town): #check if fire reached the town
         reached_town[0] = is_town_reached(grid)
         if np.any(reached_town):
@@ -226,7 +237,7 @@ def setup_ignite_probabilities_grid(grid):
 
     ignite_grid += 1
     ignite_grid *= P_0
-    ignite_grid *= 0.2
+
     return ignite_grid
 
 
